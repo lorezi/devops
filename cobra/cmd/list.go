83e12aa8a2d8c3c1611f21cbecc3946f3d00b8d3
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2022-09-12 11:34:57 +0100

    add cmds lookpath

--- cobra/cmd/list.go
+++ cobra/cmd/list.go
@@ -22,30 +22,55 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/lorezi/cobra/scan"
 	"github.com/spf13/cobra"
 )
 
-// hostsCmd represents the hosts command
-var hostsCmd = &cobra.Command{
-	Use:   "hosts",
-	Short: "Manage the hosts list",
-	Long: `Manages the hosts lists for iScan
-	
-	Add hosts with the add command
-	Delete hosts with the delete command
-	List hosts with the list command.`,
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:     "list",
+	Aliases: []string{"l"},
+	Short:   "List hosts in hosts list",
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+		hostsFile, err := cmd.Flags().GetString("hosts-file")
+		if err != nil {
+			return err
+		}
+		return listAction(os.Stdout, hostsFile, args)
+	},
 }
 
 func init() {
-	rootCmd.AddCommand(hostsCmd)
+	hostsCmd.AddCommand(listCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// hostsCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// hostsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func listAction(out io.Writer, hostFile string, args []string) error {
+	hl := &scan.HostList{}
+
+	if err := hl.Load(hostFile); err != nil {
+		return err
+	}
+
+	for _, h := range hl.Hosts {
+		if _, err := fmt.Fprintln(out, h); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
